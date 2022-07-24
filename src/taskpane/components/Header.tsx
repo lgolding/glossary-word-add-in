368f0aab28be1ac17b7bab68009ac295222d15ff
Larry Golding <laurenceg@crosslaketech.com>
2022-07-23 17:21:33 -0700

    Make Header a function component.

--- src/taskpane/components/Header.tsx
+++ src/taskpane/components/Header.tsx
@@ -6,15 +6,11 @@ export interface HeaderProps {
   message: string;
 }
 
-export default class Header extends React.Component<HeaderProps> {
-  render() {
-    const { title, logo, message } = this.props;
+const Header: React.FC<HeaderProps> = ({ title, logo, message }) => (
+  <section className="ms-welcome__header ms-bgColor-neutralLighter ms-u-fadeIn500">
+    <img width="90" height="90" src={logo} alt={title} title={title} />
+    <h1 className="ms-fontSize-su ms-fontWeight-light ms-fontColor-neutralPrimary">{message}</h1>
+  </section>
+);
 
-    return (
-      <section className="ms-welcome__header ms-bgColor-neutralLighter ms-u-fadeIn500">
-        <img width="90" height="90" src={logo} alt={title} title={title} />
-        <h1 className="ms-fontSize-su ms-fontWeight-light ms-fontColor-neutralPrimary">{message}</h1>
-      </section>
-    );
-  }
-}
+export default Header;
