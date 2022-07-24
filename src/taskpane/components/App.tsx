defec7c0860f351b03429b1b2c54d291acfb8bcc
Larry Golding <laurenceg@crosslaketech.com>
2022-07-24 10:45:24 -0700

    Make App a function component.

--- src/taskpane/components/App.tsx
+++ src/taskpane/components/App.tsx
@@ -11,38 +11,27 @@ export interface AppProps {
   isOfficeInitialized: boolean;
 }
 
-export interface AppState {
-  listItems: HeroListItem[];
-}
+const App: React.FC<AppProps> = ({ title, isOfficeInitialized }) => {
+  var [listItems, setListItems] = React.useState<HeroListItem[]>([]);
 
-export default class App extends React.Component<AppProps, AppState> {
-  constructor(props: any, context: any) {
-    super(props, context);
-    this.state = {
-      listItems: [],
-    };
-  }
+  React.useEffect(() => {
+    setListItems([
+      {
+        icon: "Ribbon",
+        primaryText: "Achieve more with Office integration",
+      },
+      {
+        icon: "Unlock",
+        primaryText: "Unlock features and functionality",
+      },
+      {
+        icon: "Design",
+        primaryText: "Create and visualize like a pro",
+      },
+    ]);
+  }, []);
 
-  componentDidMount() {
-    this.setState({
-      listItems: [
-        {
-          icon: "Ribbon",
-          primaryText: "Achieve more with Office integration!!!!",
-        },
-        {
-          icon: "Unlock",
-          primaryText: "Unlock features and functionality",
-        },
-        {
-          icon: "Design",
-          primaryText: "Create and visualize like a pro",
-        },
-      ],
-    });
-  }
-
-  click = async () => {
+  const click = async () => {
     return Word.run(async (context) => {
       /**
        * Insert your Word code here
@@ -58,31 +47,29 @@ export default class App extends React.Component<AppProps, AppState> {
     });
   };
 
-  render() {
-    const { title, isOfficeInitialized } = this.props;
-
-    if (!isOfficeInitialized) {
-      return (
-        <Progress
-          title={title}
-          logo={require("./../../../assets/logo-filled.png")}
-          message="Please sideload your addin to see app body."
-        />
-      );
-    }
-
+  if (!isOfficeInitialized) {
     return (
-      <div className="ms-welcome">
-        <Header logo={require("./../../../assets/logo-filled.png")} title={this.props.title} message="Welcome" />
-        <HeroList message="Discover what Office Add-ins can do for you today!" items={this.state.listItems}>
-          <p className="ms-font-l">
-            Modify the source files, then click <b>Run</b>.
-          </p>
-          <DefaultButton className="ms-welcome__action" iconProps={{ iconName: "ChevronRight" }} onClick={this.click}>
-            Run
-          </DefaultButton>
-        </HeroList>
-      </div>
+      <Progress
+        title={title}
+        logo={require("./../../../assets/logo-filled.png")}
+        message="Please sideload your add-in to see app body."
+      />
     );
   }
-}
+
+  return (
+    <div className="ms-welcome">
+      <Header logo={require("./../../../assets/logo-filled.png")} title={title} message="Welcome" />
+      <HeroList message="Discover what Office Add-ins can do for you today!" items={listItems}>
+        <p className="ms-font-l">
+          Modify the source files, then click <b>Run</b>.
+        </p>
+        <DefaultButton className="ms-welcome__action" iconProps={{ iconName: "ChevronRight" }} onClick={click}>
+          Run
+        </DefaultButton>
+      </HeroList>
+    </div>
+  );
+};
+
+export default App;
