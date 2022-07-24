e29a09a12d70e5d6b0976cd4591a96744f8d8753
Larry Golding <laurenceg@crosslaketech.com>
2022-07-23 17:34:19 -0700

    Make Progress a function component.

--- src/taskpane/components/Progress.tsx
+++ src/taskpane/components/Progress.tsx
@@ -7,16 +7,12 @@ export interface ProgressProps {
   title: string;
 }
 
-export default class Progress extends React.Component<ProgressProps> {
-  render() {
-    const { logo, message, title } = this.props;
+const Progress: React.FC<ProgressProps> = ({ logo, message, title }) => (
+  <section className="ms-welcome__progress ms-u-fadeIn500">
+    <img width="90" height="90" src={logo} alt={title} title={title} />
+    <h1 className="ms-fontSize-su ms-fontWeight-light ms-fontColor-neutralPrimary">{title}</h1>
+    <Spinner size={SpinnerSize.large} label={message} />
+  </section>
+);
 
-    return (
-      <section className="ms-welcome__progress ms-u-fadeIn500">
-        <img width="90" height="90" src={logo} alt={title} title={title} />
-        <h1 className="ms-fontSize-su ms-fontWeight-light ms-fontColor-neutralPrimary">{title}</h1>
-        <Spinner size={SpinnerSize.large} label={message} />
-      </section>
-    );
-  }
-}
+export default Progress;
